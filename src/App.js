2ac095293f2d37e9c65ade4b311db4f9976b8889
gauthmz <gauthambabu12@gmail.com>
2024-04-02 20:24:44 +0530

    Search Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
+import SearchRecipe from './components/SearchRecipe';
 
 function App() {
   return (
     <div>
       <AddRecipe />
+      <SearchRecipe/>
     </div>
   );
 }
