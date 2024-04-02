04edd6935ce51d1a0d00ce498265dc74369eb026
gauthmz <gauthambabu12@gmail.com>
2024-04-02 20:10:45 +0530

    Add Recipe Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddRecipe from './components/AddRecipe';
 
 function App() {
   return (
     <div>
-    
+      <AddRecipe />
     </div>
   );
 }
